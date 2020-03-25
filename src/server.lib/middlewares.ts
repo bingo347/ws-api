01d98bf1ddbd5f0b227d62b91e8a36b625818c7c
Dmitriy <mail@d-belyaev.ru>
2020-03-25 06:58:45 +0300

    realised useMiddleware

--- src/server.lib/middlewares.ts
+++ src/server.lib/middlewares.ts
@@ -1,5 +1,7 @@
 import {ApiServerContext} from './context';
 import {ApiServer} from './apiServer';
+import {normalizeArgs} from './helpers';
+import {middlewares} from './symbols';
 
 export type Middleware = (
     this: ApiServerContext,
@@ -8,7 +10,12 @@ export type Middleware = (
 
 export function useMiddleware<Server extends ApiServer<any, any>>(server: Server, middleware: Middleware): Server;
 export function useMiddleware<Server extends ApiServer<any, any>>(middleware: Middleware): (server: Server) => Server;
-export function useMiddleware<Server extends ApiServer<any, any>>(...args: [Server | Middleware, Middleware?]): Server | ((server: Server) => Server) {
-    // TODO:
-    return void args as any as Server;
+export function useMiddleware<Server extends ApiServer<any, any>>(...args: [Server, Middleware] | [Middleware]): Server | ((server: Server) => Server) {
+    const [server, middleware] = normalizeArgs(args);
+    const bindMiddelware = createMiddlewareBinder<Server>(middleware);
+    return server ? bindMiddelware(server) : bindMiddelware;
+}
+
+function createMiddlewareBinder<Server extends ApiServer<any, any>>(middleware: Middleware) {
+    return (server: Server) => (server[middlewares](prevMiddlewares => [...prevMiddlewares, middleware]), server);
 }
