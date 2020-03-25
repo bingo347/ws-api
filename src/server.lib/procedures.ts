b084c7e446cf42db65e91575ad0e06a2d88e5a94
Dmitriy <mail@d-belyaev.ru>
2020-03-25 14:51:10 +0300

    realised mountProcedure

--- src/server.lib/procedures.ts
+++ src/server.lib/procedures.ts
@@ -2,6 +2,8 @@ import {Readable as NodeReadableStream} from 'stream';
 import {ProceduresBase} from '../shared/communications';
 import {ApiServerContext} from './context';
 import {ApiServer} from './apiServer';
+import {normalizeArgs} from './helpers';
+import {procedures} from './symbols';
 
 export type Procedure<Payload, Result> = (
     this: ApiServerContext,
@@ -28,7 +30,26 @@ export function mountProcedure<
     ProceduresInfo extends ProceduresBase,
     ProcedureName extends keyof ProceduresInfo,
     Server extends ApiServer<Procedures<ProceduresInfo>, any> = ApiServer<Procedures<ProceduresInfo>, any>
->(...args: [Server | ProcedureName, ProcedureName | Procedures<ProceduresInfo>[ProcedureName], Procedures<ProceduresInfo>[ProcedureName]?]): Server | ((server: Server) => Server) {
-    // TODO:
-    return void args as any as Server;
+>(...args: [Server, ProcedureName, Procedures<ProceduresInfo>[ProcedureName]] | [ProcedureName, Procedures<ProceduresInfo>[ProcedureName]]): Server | ((server: Server) => Server) {
+    const [server, procedureName, procedure] = normalizeArgs(args);
+    const bindProcedure = createProcedureBinder<ProceduresInfo, ProcedureName, Server>(procedureName, procedure);
+    return server ? bindProcedure(server) : bindProcedure;
+}
+
+function createProcedureBinder<
+    ProceduresInfo extends ProceduresBase,
+    ProcedureName extends keyof ProceduresInfo,
+    Server extends ApiServer<Procedures<ProceduresInfo>, any>
+>(procedureName: ProcedureName, procedure: Procedures<ProceduresInfo>[ProcedureName]) {
+    return (server: Server) => {
+        if(procedureName in server[procedures]()) {
+            // eslint-disable-next-line fp/no-throw
+            throw new Error(`Cannot mount procedure \`${procedureName}\` to the some server twice`);
+        }
+        server[procedures](prevProcedures => ({
+            ...prevProcedures,
+            [procedureName]: procedure
+        }));
+        return server;
+    };
 }
