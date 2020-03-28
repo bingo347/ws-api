0aa5fa58376f47efa2654a07b03f0737a6933090
Dmitriy <mail@d-belyaev.ru>
2020-03-28 17:41:00 +0300

    realise createServer

--- src/server.lib/apiServer.ts
+++ src/server.lib/apiServer.ts
@@ -1,23 +1,25 @@
+import {EventEmitter} from 'events';
 import {Server as HTTPServer} from 'http';
 import {Server as HTTPSServer} from 'https';
-import {ExtensionCodec} from '@msgpack/msgpack';
 import WebSocket from 'ws';
 import {ProceduresBase, ChannelsBase} from '../shared/communications';
+import {PatchedExtensionCodec, defaultExtensionCodec} from '../shared/msgpack';
 import {Middleware} from './middlewares';
 import {Procedures} from './procedures';
 import {Channels} from './channels';
 import {middlewares, procedures, channels} from './symbols';
-import {Store} from './helpers';
+import {Store, createStore} from './helpers';
+import {makeNewContextListener} from './context';
 
 export type ApiServerOptions<CodecContextType> = Omit<WebSocket.ServerOptions, 'server'> & {
     server?: WebSocket.Server | HTTPServer | HTTPSServer;
-    extensionCodec?: ExtensionCodec<CodecContextType>;
+    extensionCodec?: PatchedExtensionCodec<CodecContextType>;
 };
 
 export type ApiServer<
     P extends Procedures<ProceduresBase>,
     C extends Channels<ChannelsBase>
-> = {
+> = EventEmitter & {
     [middlewares]: Store<Middleware[]>;
     [procedures]: Store<Partial<P>>;
     [channels]: Store<Partial<C>>;
@@ -27,8 +29,47 @@ export type ApiServer<
 export function createServer<
     ProceduresInfo extends ProceduresBase,
     ChannelsInfo extends ChannelsBase,
-    CodecContextType = void
->(options?: ApiServerOptions<CodecContextType>): ApiServer<Procedures<ProceduresInfo>, Channels<ChannelsInfo>> {
-    // TODO:
-    return void options as any as ApiServer<Procedures<ProceduresInfo>, Channels<ChannelsInfo>>;
+    CodecContextType = undefined
+>(options: ApiServerOptions<CodecContextType> = {}) {
+    const wsServer = extractWSServer(options);
+    const extensionCodec = extractExtensionCodec(options);
+    // eslint-disable-next-line fp/no-mutating-assign
+    const apiServer = Object.assign(new EventEmitter(), {
+        [middlewares]: createStore<Middleware[]>([]),
+        [procedures]: createStore<Partial<ProceduresInfo>>({}),
+        [channels]: createStore<Partial<ChannelsInfo>>({}),
+        close: wrapClose(wsServer)
+    }) as ApiServer<Procedures<ProceduresInfo>, Channels<ChannelsInfo>>;
+    proxyWSServerEvents(wsServer, apiServer).on('connection', makeNewContextListener(apiServer, extensionCodec));
+    return apiServer;
+}
+
+function extractWSServer(options: ApiServerOptions<any>): WebSocket.Server {
+    return (options.server instanceof WebSocket.Server
+        ? options.server
+        : new WebSocket.Server(options)
+    );
+}
+
+function extractExtensionCodec<CodecContextType>(options: ApiServerOptions<CodecContextType>): PatchedExtensionCodec<CodecContextType> {
+    return (options.extensionCodec instanceof PatchedExtensionCodec
+        ? options.extensionCodec
+        : defaultExtensionCodec as PatchedExtensionCodec<any>
+    );
+}
+
+function wrapClose(wss: WebSocket.Server) {
+    return () => new Promise(
+        (resolve, reject) => wss.close(
+            err => (err ? reject(err) : resolve())
+        )
+    );
+}
+
+function proxyWSServerEvents(wss: WebSocket.Server, apiServer: EventEmitter) {
+    return (wss
+        .on('error', (...args) => apiServer.emit('error', ...args))
+        .on('listening', (...args) => apiServer.emit('listening', ...args))
+        .on('headers', (...args) => apiServer.emit('headers', ...args))
+    );
 }
--- src/server.lib/context.ts
+++ src/server.lib/context.ts
@@ -1,9 +1,39 @@
 import {IncomingMessage} from 'http';
+import WebSocket from 'ws';
+import {ProceduresBase, ChannelsBase} from '../shared/communications';
+import {ApiServer} from './apiServer';
+import {Procedures} from './procedures';
+import {Channels} from './channels';
+import {PatchedExtensionCodec} from '../shared/msgpack';
+import {Store, createStore} from './helpers';
 
 export type ApiServerContext = {
     socket: WebSocket;
     request: IncomingMessage;
-    close(code: number, reason: string): void;
-    // TODO:
-    session: {};
+    session: Store<Record<string | symbol, any>>;
+    close(code?: number, reason?: string): void;
 };
+
+export function makeNewContextListener<
+    ProceduresInfo extends ProceduresBase,
+    ChannelsInfo extends ChannelsBase,
+    CodecContextType = undefined
+>(
+    apiServer: ApiServer<Procedures<ProceduresInfo>, Channels<ChannelsInfo>>,
+    extensionCodec: PatchedExtensionCodec<CodecContextType>
+) {
+    return (socket: WebSocket, request: IncomingMessage) => {
+        const ctx = createContext(socket, request);
+        // TODO:
+        return void [ctx, apiServer, extensionCodec];
+    };
+}
+
+function createContext(socket: WebSocket, request: IncomingMessage): ApiServerContext {
+    return {
+        socket,
+        request,
+        session: createStore({}),
+        close: (code, reason) => socket.close(code, reason)
+    };
+}
