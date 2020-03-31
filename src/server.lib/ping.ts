77f532594e3a92c054ab482002fd1e303bb6f1e3
Dmitriy <mail@d-belyaev.ru>
2020-03-31 16:34:16 +0300

    add createPinger

---
+++ src/server.lib/ping.ts
@@ -0,0 +1,68 @@
+import {
+    Communication,
+    createCommunication,
+    PING
+} from '../shared/communications';
+import {createStore, noop, Store} from './helpers';
+
+type PongAwaiter = {
+    resolve(): void;
+    timer: void | NodeJS.Timeout;
+};
+type PromiseExecutor = (resolve: () => void, reject: (reason?: any) => void) => void;
+
+const pong = Symbol('pong');
+const getInitialPongAwaiterValue = (): PongAwaiter => ({resolve: noop, timer: void 0});
+const makePong = (pongAwaiter: Store<PongAwaiter>) => () => (
+    pongAwaiter().resolve(),
+    pongAwaiter(getInitialPongAwaiterValue),
+    void 0
+);
+const getLastResolve = (pongAwaiter: Store<PongAwaiter>) => {
+    const {timer, resolve} = pongAwaiter();
+    timer && clearTimeout(timer);
+    return resolve;
+};
+const wrapPromiseExecutor = (
+    lastResolve: () => void,
+    executor: PromiseExecutor
+): PromiseExecutor => (lastResolve === noop
+    ? executor
+    : ((resolve, reject) => executor(() => (lastResolve(), resolve()), reject))
+);
+const rejectOnTimeout = (
+    timeout: number,
+    reject: (reason?: any) => void
+) => setTimeout(() => reject(new Error('pong timeout')), timeout);
+const makePongAwaiterUpdate = (
+    timeout: number,
+    resolve: () => void,
+    reject: (reason?: any) => void
+) => (timeout > 0
+    ? (() => ({resolve, timer: rejectOnTimeout(timeout, reject)}))
+    : (() => ({resolve, timer: void 0}))
+);
+const makePongAwaiterPromiseExecutor = (
+    timeout: number,
+    pongAwaiter: Store<PongAwaiter>,
+    sender: (data: Communication) => void
+) => wrapPromiseExecutor(
+    getLastResolve(pongAwaiter),
+    (resolve, reject) => (
+        pongAwaiter(makePongAwaiterUpdate(timeout, resolve, reject)),
+        sender(createCommunication(PING))
+    )
+);
+
+export function createPinger(sender: (data: Communication) => void) {
+    const pongAwaiter = createStore<PongAwaiter>(getInitialPongAwaiterValue());
+    return Object.assign((timeout = -1) => (
+        new Promise(
+            makePongAwaiterPromiseExecutor(timeout, pongAwaiter, sender)
+        ).finally(
+            () => pongAwaiter(getInitialPongAwaiterValue)
+        )
+    ), {
+        [pong]: makePong(pongAwaiter)
+    });
+}
