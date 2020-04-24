c5c2fb4ba12756672564c004ce83e150a7f569b8
Dmitriy <mail@d-belyaev.ru>
2020-04-24 20:06:16 +0300

    add pipe helper

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -17,7 +17,10 @@ export function createStore<T>(initValue: T) {
     return store;
 }
 
+export type Fn<Args extends any[] = any[], Result extends any = any> = (...args: Args) => Result;
+
 export const noop = () => void 0;
+export const identity = <T>(x: T) => x;
 
 export function partial<T0, TS extends any[], R>(
     fn: (x0: T0, ...xs: TS) => R,
@@ -45,4 +48,32 @@ export function partial<T0, T1, T2, T3, T4, T5, TS extends any[], R>(
 ): (...xs: TS) => R;
 export function partial(fn: (...args: any[]) => any, ...head: any[]) {
     return (...tail: any[]) => fn(...head, ...tail);
+}
+
+export function pipe<F0 extends Fn>(f0: F0): Fn<Parameters<F0>, ReturnType<F0>>;
+export function pipe<
+    F0 extends Fn,
+    F1 extends Fn<[ReturnType<F0>]>
+>(f0: F0, f1: F1): Fn<Parameters<F0>, ReturnType<F1>>;
+export function pipe<
+    F0 extends Fn,
+    F1 extends Fn<[ReturnType<F0>]>,
+    F2 extends Fn<[ReturnType<F1>]>
+>(f0: F0, f1: F1, f2: F2): Fn<Parameters<F0>, ReturnType<F2>>;
+export function pipe<
+    F0 extends Fn,
+    F1 extends Fn<[ReturnType<F0>]>,
+    F2 extends Fn<[ReturnType<F1>]>,
+    F3 extends Fn<[ReturnType<F2>]>
+>(f0: F0, f1: F1, f2: F2, f3: F3): Fn<Parameters<F0>, ReturnType<F3>>;
+export function pipe<
+    F0 extends Fn,
+    F1 extends Fn<[ReturnType<F0>]>,
+    F2 extends Fn<[ReturnType<F1>]>,
+    F3 extends Fn<[ReturnType<F2>]>,
+    F4 extends Fn<[ReturnType<F3>]>
+>(f0: F0, f1: F1, f2: F2, f3: F3, f4: F4): Fn<Parameters<F0>, ReturnType<F4>>;
+export function pipe(fn: Fn, ...fns: Fn<[any]>[]) {
+    const piped = fns.reduce((prev, curr) => x => curr(prev(x)), identity);
+    return (...args: any[]) => piped(fn(...args));
 }
