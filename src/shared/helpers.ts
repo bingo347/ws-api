5472cc69ffcae42932931665e695808f9190f948
Dmitriy <mail@d-belyaev.ru>
2020-04-24 17:25:09 +0300

    add partial helper

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -18,3 +18,31 @@ export function createStore<T>(initValue: T) {
 }
 
 export const noop = () => void 0;
+
+export function partial<T0, TS extends any[], R>(
+    fn: (x0: T0, ...xs: TS) => R,
+    x0: T0
+): (...xs: TS) => R;
+export function partial<T0, T1, TS extends any[], R>(
+    fn: (x0: T0, x1: T1, ...xs: TS) => R,
+    x0: T0, x1: T1
+): (...xs: TS) => R;
+export function partial<T0, T1, T2, TS extends any[], R>(
+    fn: (x0: T0, x1: T1, x2: T2, ...xs: TS) => R,
+    x0: T0, x1: T1, x2: T2
+): (...xs: TS) => R;
+export function partial<T0, T1, T2, T3, TS extends any[], R>(
+    fn: (x0: T0, x1: T1, x2: T2, x3: T3, ...xs: TS) => R,
+    x0: T0, x1: T1, x2: T2, x3: T3
+): (...xs: TS) => R;
+export function partial<T0, T1, T2, T3, T4, TS extends any[], R>(
+    fn: (x0: T0, x1: T1, x2: T2, x3: T3, x4: T4, ...xs: TS) => R,
+    x0: T0, x1: T1, x2: T2, x3: T3, x4: T4
+): (...xs: TS) => R;
+export function partial<T0, T1, T2, T3, T4, T5, TS extends any[], R>(
+    fn: (x0: T0, x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, ...xs: TS) => R,
+    x0: T0, x1: T1, x2: T2, x3: T3, x4: T4, x5: T5
+): (...xs: TS) => R;
+export function partial(fn: (...args: any[]) => any, ...head: any[]) {
+    return (...tail: any[]) => fn(...head, ...tail);
+}
